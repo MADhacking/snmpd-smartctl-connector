639e0d9cdfdd955d5ebc8389ca42f096a25b5923
Max Hacking <max@hacking.co.uk>
2019-11-21 16:26:58 +0000

    coverage.sh no longer needs to create a user or use su.

--- .gentoo/coverage.sh
+++ .gentoo/coverage.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-useradd -m -G users,portage -s /bin/bash testrunner
-
-su --preserve-environment testrunner -c \
-    "kcov --bash-dont-parse-binary-dir \
+kcov --bash-dont-parse-binary-dir \
      --include-path=usr \
      /var/tmp/coverage \
-     bats -t tests"
+     bats -t tests
