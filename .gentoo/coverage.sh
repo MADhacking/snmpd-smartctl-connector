d6163f901a6f91b331842beb0423fee888a8dd18
Max Hacking <max@hacking.co.uk>
2019-11-21 00:54:12 +0000

    Fix coverage script?

--- .gentoo/coverage.sh
+++ .gentoo/coverage.sh
@@ -4,6 +4,6 @@ useradd -m -G users,portage -s /bin/bash testrunner
 
 su --preserve-environment testrunner -c \
     "kcov --bash-dont-parse-binary-dir \
-     --include-path=usr/ \
+     --include-path=usr \
      /var/tmp/coverage \
      bats -t tests"
